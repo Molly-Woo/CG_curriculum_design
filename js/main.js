9d82849b9234b61baf800aef091001ed64ef1268
pollia <3152668032@qq.com>
2020-12-25 01:22:10 +0800

    示例model引入成功

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 import * as THREE from './three.module.js';
 
 import { OrbitControls } from './OrbitControls.js';
+import { GLTFLoader } from './GLTFLoader.js';
 
-let controls, camera, scene, renderer;
+let controls, camera, scene, model,renderer;
 let textureCube;
 let sphereMesh, sphereMaterial;
 var group = new THREE.Group();
@@ -40,6 +41,18 @@ function init() {
     textureCube.mapping = THREE.CubeRefractionMapping;
 
     //
+    const loader2 = new GLTFLoader();
+    loader2.load( 'models/RobotExpressive.glb', function ( gltf ) {
+
+        model = gltf.scene;
+        model.scale.set(100, 100, 100);
+        console.log(model);
+        scene.add( model );
+        console.log(scene);
+
+    }, undefined, function ( e ) {
+        console.error( e );
+    } );
 
     const geometry = new THREE.IcosahedronBufferGeometry(600, 15);
     sphereMaterial = new THREE.MeshLambertMaterial({ envMap: textureCube, opacity: 0.7, transparent: true });
@@ -48,7 +61,9 @@ function init() {
 
     var geometry1 = new THREE.BoxGeometry(50, 50, 50);
     var material1 = new THREE.MeshLambertMaterial({
-        color: 0x0000ff
+        color: 0x0000ff,
+        opacity: 0.7,
+        transparent: true
     }); //材质对象Material
     var mesh1 = new THREE.Mesh(geometry1, material1); //网格模型对象Mesh
     scene.add(mesh1); //网格模型添加到场景中
